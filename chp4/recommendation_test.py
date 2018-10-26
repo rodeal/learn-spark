fb51517c45664b4501f50a5a3a002a4d0ffc4cb6
yuanyu <379855721a@gmail.com>
2018-10-26 17:54:20 +0800

    update recommendation system

--- chp4/recommendation_test.py
+++ chp4/recommendation_test.py
@@ -10,6 +10,8 @@ findspark.init()
 from pyspark import SparkContext
 from pyspark.mllib.recommendation import ALS
 from pyspark.mllib.recommendation import Rating
+import numpy as np
+from numpy import linalg as LA
 
 sc = SparkContext("local[4]", "First Spark App")
 
@@ -18,6 +20,9 @@ def to_implicit(x):
         return 0
     else:
         return 1
+
+def cosineSimilarity(vec1, vec2):
+    return float((np.dot(vec1, vec2) / (LA.norm(vec1) * LA.norm(vec2))))
     
 #转换为Rating类
 rawData = sc.textFile("../data/ml-100k/u.data")
@@ -49,6 +54,7 @@ print("用户数量:", user_num)
 print("电影数量:", movie_num)
 
 """
+PART I 用户推荐
 模型应用:
 预测：model.predict()
 输出某用户的前k个推荐：model.recomendProducts(user, num)
@@ -59,3 +65,28 @@ userId = 789
 K = 10
 topKRecs = model.recommendProducts(userId, K)
 print(topKRecs)
+
+"""
+模型检验
+"""
+movies = sc.textFile("../data/ml-100k/u.item")
+titles = movies.map(lambda lines: lines.split("|")[:2]).map(lambda items: (int(items[0]), items[1])).collectAsMap()
+#titles
+moviesForUser = ratings.keyBy(lambda x: x.user).lookup(789)
+print(len(moviesForUser))
+sorted_ratings = sc.parallelize(moviesForUser).sortBy(lambda x: x.rating, ascending=False).take(10)
+top10movies = [(titles[rating.product], rating.rating) for rating in sorted_ratings ]
+top10recomm = [(titles[rating.product], rating.rating) for rating in topKRecs]
+
+"""
+Part II 物品推荐
+"""
+itemId = 567
+itemFactor = model.productFeatures().lookup(itemId)[0].tolist()
+itemVector = np.matrix(itemFactor).T
+cosineSimilarity(itemVector.T, itemVector)
+sims = model.productFeatures().map(lambda items: (items[0], cosineSimilarity(np.matrix(items[1].tolist()), itemVector)))
+sortedSims = sims.top(K, key=lambda x: x[1])
+sortedSims2 = sims.top(K+1, key=lambda x: x[1])
+similar_titles = [(titles[x[0]], x[1]) for x in sortedSims2[1:11]]
+
